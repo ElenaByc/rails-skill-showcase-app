16a99ff44823ae0086994429de2a00e6e50f4bfa
Elena Bychenkova <elena.bychenkova@gmail.com>
2025-10-23 11:30:11 -0700

    Install Brakeman, fix Gemfile platform names and add certificate URL validation

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -4,4 +4,8 @@ class Certificate < ApplicationRecord
 
   has_many :certificate_skills
   has_many :skills, through: :certificate_skills
+
+  validates :name, presence: true
+  validates :issued_on, presence: true
+  validates :verification_url, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]), allow_blank: true }
 end
